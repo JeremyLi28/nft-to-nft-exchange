e4a520f375cfa5fb96c73735b3dc000467ec8710
Chen Li <jeremyliisagoodman@gmail.com>
2022-01-30 15:31:04 -0800

    Add basic nft contract

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/DH6MrVIydt64y-wLIWuVxgnwU74yEzRE',
+      accounts: ['8abe7b556257aaf0e91c0ed0e48e9e5c1b6081281b29e3d964ac4db0671e6573'],
+    },
+  },
 };
